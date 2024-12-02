81037d9cad008a52537b517dff2dac34109bdbd9
Riricola <71103639+Riricola@users.noreply.github.com>
2024-12-02 11:44:39 -0600

    Update blackjack.c
    after project working in class on monday

--- blackjack.c
+++ blackjack.c
@@ -10,8 +10,8 @@ Authors:
 #include <stdbool.h>
 #include <time.h>
 
-// #include "message.h"
-// #include "socket.h"
+#include "message.h"
+#include "socket.h"
 
 #define NUM_SUITS 4
 #define NUM_CARDS 13
@@ -40,7 +40,7 @@ typedef struct hand {
 
 typedef struct player {
     int score; // how many games won
-    int Id; //player ID (aka thread id)
+    int Id; //player ID (aka client id)
     int luck; //keeps track of advantages/disadvantages
     hand_t phand; //the players hand
     bool stay;  // did the player stay or did they hit? Scheduler checks this, if TRUE, skip players turn
@@ -48,6 +48,13 @@ typedef struct player {
 
 
 card_t deck[52];
+
+//Array that holds the players
+player_t players[4];
+
+//Global that makes array spot different each time
+//Might have to put a lock around this?
+int playNumber;
 
 
 /**
@@ -80,19 +87,23 @@ void deck_init () {
 /**
     Initalize player. 
  */
- void player_init(player_t player) {
+ void player_init(player_t player, void* playerId) {
+    int client_socket_fd = * (int*)playerId;
     player.score = 0;
-    //player.Id = pthread_self();
+    player.Id = client_socket_fd;
     player.luck = 0;
     hand_init(player);
-    
+    //Add the player to the array of players
+    players[playNumber] = player;
+    //increment playnumber so that next player gets sent to next spot in array
+    playNumber++;
  }         
  
 
 /**
     Draw cards
  */
-void draw_cards(player_t player){
+card_t draw_cards(int playerId){
   // Seed the random number generator with a known starting point
   srand(time(NULL)); // 
   int pick = rand() % 52;
@@ -101,13 +112,17 @@ void draw_cards(player_t player){
     } // while
     
     deck[pick].picked = true;
+    card_t drawnCard;
+    drawnCard.digit = pick/4 + 1;
+    drawnCard.suit = pick % 4 + 1;
   // after drawing a card, print it so we can see if it is random each time
   printf("%d \n", deck[pick].digit);
 
   // add the card to the player's hand total
-  player.phand.total += deck[pick].digit;
+  //player.phand.total += deck[pick].digit;
   //card[] card_hand = player.phand.cards
   // 
+  return drawnCard;
 }
 
 void print_deck(){
@@ -121,7 +136,113 @@ void print_deck(){
     }
 }
 
+void * handle_connection(void* arg) {
+  int client_socket_fd = * (int*)arg;
+  free(arg);
+  
+  typedef struct startingHand{
+    card_t card1;
+    card_t card2;
+  } startingHand_t;
+
+  startingHand_t message;
+
+  message.card1 = draw_cards(client_socket_fd);
+  message.card2 = draw_cards(client_socket_fd);
+
+int nbytes;
+
+// Send a struct to the client containing their starting hand
+if ((nbytes = write(client_socket_fd, &message, sizeof(startingHand_t)) != sizeof(message)))
+{
+  printf("Error writing my message");
+}
+
+  while (1) {
+    // Read a message from the client
+    char* cmessage = receive_message(client_socket_fd);
+    if (cmessage == NULL) {
+      perror("Failed to read message from client");
+      break;
+    }
+    if(strcmp(cmessage, "stay") == 0)
+    {
+        break;
+    }
+    else if (strcmp(cmessage, "hit") == 0)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if(players[i].Id == client_socket_fd)
+            {
+              players[i].phand
+            }
+        }
+        draw_cards(client_socket_fd);
+        continue;
+    }
+    else {
+        char * m = "Please enter a valid result: Either 'hit' or 'stay'";
+        int rc = send_message(client_socket_fd, m);
+        if (rc == -1) {
+            perror("Failed to send message to client");
+            continue;
+        }
+    }
+  }
+
+
+  close(client_socket_fd);
+  return NULL;
+}
+
+
+
 int main(){
+  // Open a server socket
+  unsigned short port = 0;
+  int server_socket_fd = server_socket_open(&port);
+  if (server_socket_fd == -1) {
+    perror("Server socket was not opened");
+    exit(EXIT_FAILURE);
+  }
+
+  // Start listening for connections, with a maximum of one queued connection
+  if (listen(server_socket_fd, 1)) {
+    perror("listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  while(1) {
+    printf("Server listening on port %u\n", port);
+  
+    // Wait for a client to connect
+    int client_socket_fd = server_socket_accept(server_socket_fd);
+    if (client_socket_fd == -1) {
+      perror("accept failed");
+      break;
+    }
+
+    printf("Client connected!\n");
+
+    pthread_t thread;
+
+    int * threadarg = malloc(sizeof(int));
+    *threadarg = client_socket_fd;
+
+    pthread_create(&thread, NULL, handle_connection, threadarg);    
+  }
+
+  
+
+  // Close sockets
+  close(server_socket_fd);
+
+  return 0;
+
+
+
+
     deck_init();
     draw_cards();
     draw_cards();
@@ -172,3 +293,4 @@ Networking
 
     
  */
+
